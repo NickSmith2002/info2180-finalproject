ee01f9e83f57dfa0f4ca187ad962db99ac081e23
Gashara <99560334+Gashara@users.noreply.github.com>
2023-12-06 17:19:42 -0800

    Add files via upload

---
+++ dolphin_crm.sql
@@ -0,0 +1,36 @@
+CREATE TABLE Users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    password VARCHAR(255),
+    email VARCHAR(255),
+    role VARCHAR(255),
+    created_at DATETIME
+);
+
+CREATE TABLE Contacts (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    firstname VARCHAR(255),
+    lastname VARCHAR(255),
+    email VARCHAR(255),
+    telephone VARCHAR(255),
+    company VARCHAR(255),
+    type VARCHAR(255),
+    assigned_to INTEGER,
+    created_by INTEGER,
+    created_at DATETIME,
+    updated_at DATETIME,
+    FOREIGN KEY (assigned_to) REFERENCES Users(id),
+    FOREIGN KEY (created_by) REFERENCES Users(id)
+);
+
+CREATE TABLE Notes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    contact_id INTEGER,
+    comment TEXT,
+    created_by INTEGER,
+    created_at DATETIME,
+    FOREIGN KEY (contact_id) REFERENCES Contacts(id),
+    FOREIGN KEY (created_by) REFERENCES Users(id)
+);
