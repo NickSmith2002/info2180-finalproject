2498db2a69aaf82c5b53726f227b1f51a6a8d830
Calvin Stephenson <cstephenson882@gmail.com>
2023-12-09 21:57:56 +0000

    Added the link to the buttons in the dashboard.html

--- dolphin_crm.sql
+++ dolphin_crm.sql
@@ -38,3 +38,6 @@ CREATE TABLE Notes (
 
 INSERT INTO Users (firstname, lastname, password, email, role, created_at) 
 VALUES ('Admin', 'User', 'hashed_password123', 'admin@project2.com', 'admin', NOW());
+
+CREATE USER 'dolphin_crm_user'@'localhost' IDENTIFIED VIA mysql_native_password USING '***';
+GRANT ALL PRIVILEGES ON *.* TO 'dolphin_crm_user'@'localhost' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;GRANT ALL PRIVILEGES ON `dolphin\_crm`.* TO 'dolphin_crm_user'@'localhost';
